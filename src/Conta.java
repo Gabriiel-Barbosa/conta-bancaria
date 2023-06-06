c6c306250dec4000e991f105e9f40b19d056736c
Gabriel <gabrielbarbosa.j01@gmail.com>
2023-06-06 10:03:15 -0300

    Adicionado o Menu

--- src/Conta.java
+++ src/Conta.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Conta {
     public static void main(String[] args) {
         String nome = "Mark Zuckerberg";
@@ -9,5 +11,22 @@ public class Conta {
         System.out.println("Tipo conta: " + tipoConta);
         System.out.println("Saldo atual: " + saldo);
         System.out.println("\n***************************");
+
+        /*O System.in é indicar que serão lidos dados do teclado*/
+        Scanner leitura = new Scanner(System.in);
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar Valor
+                2 - Transferir Valor
+                3 - Receber Valor
+                4 - Sair
+                """;
+        int opcao = 0;
+        while (opcao != 4) {
+
+            System.out.println(menu);
+            // O nextInt diz qual tipo de dado será lido. Nesse caso é um inteiro
+            opcao = leitura.nextInt();
+        }
     }
 }
